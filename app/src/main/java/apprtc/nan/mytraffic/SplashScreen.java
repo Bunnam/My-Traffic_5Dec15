6fe6bc98db1441a5f34d5b0c737e423a44c88852
nan <nan.030539@gmail.com>
2015-12-06 14:51:20 +0700

    Soud fint

--- app/src/main/java/apprtc/nan/mytraffic/SplashScreen.java
+++ app/src/main/java/apprtc/nan/mytraffic/SplashScreen.java
@@ -1,5 +1,8 @@
 package apprtc.nan.mytraffic;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,18 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-    }
-}
+
+        Handler objHandler = new Handler();
+        objHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent obIntent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(obIntent);
+                finish();
+
+            }
+        }, 8000);
+        MediaPlayer intrMediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.intro_tata);
+
+    }//Main Method
+} //Main Class
