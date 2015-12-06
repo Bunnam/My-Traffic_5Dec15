def5de4cd1a82f9fea6d2a295dfa0115a5c17b32
nan <nan.030539@gmail.com>
2015-12-06 12:37:13 +0700

    List View list

--- app/src/main/java/apprtc/nan/mytraffic/MainActivity.java
+++ app/src/main/java/apprtc/nan/mytraffic/MainActivity.java
@@ -22,9 +22,53 @@ public class MainActivity extends AppCompatActivity {
         //Bind widget
         bindWidget();
         //Button Controller
-         buttonController();
+        buttonController();
+        //LisView Controller
+        ListViewController();
+
 
     } // Main Method
+
+    private void ListViewController() {
+        //For Icon
+        int[] intIcon = {R.drawable.traffic_01, R.drawable.traffic_02,
+                R.drawable.traffic_03, R.drawable.traffic_04,
+                R.drawable.traffic_05,R.drawable.traffic_06,
+        R.drawable.traffic_07,R.drawable.traffic_08,
+                R.drawable.traffic_09,R.drawable.traffic_10,
+                R.drawable.traffic_11,R.drawable.traffic_12,
+                R.drawable.traffic_13,R.drawable.traffic_14,
+                R.drawable.traffic_15,R.drawable.traffic_16,
+                R.drawable.traffic_17,R.drawable.traffic_18,
+                R.drawable.traffic_19,R.drawable.traffic_20};
+        //For Title
+        String[] titleString = new String[20];
+        titleString[0] = "หัวข้อที่ 1";
+        titleString[1] = "หัวข้อที่ 2";
+        titleString[2] = "หัวข้อที่ 3";
+        titleString[3] = "หัวข้อที่ 4";
+        titleString[4] = "หัวข้อที่ 5";
+        titleString[5] = "หัวข้อที่ 6";
+        titleString[6] = "หัวข้อที่ 7";
+        titleString[7] = "หัวข้อที่ 8";
+        titleString[8] = "หัวข้อที่ 9";
+        titleString[9] = "หัวข้อที่ 10";
+        titleString[10] = "หัวข้อที่ 11";
+        titleString[11] = "หัวข้อที่ 12";
+        titleString[12] = "หัวข้อที่ 13";
+        titleString[13] = "หัวข้อที่ 14";
+        titleString[14] = "หัวข้อที่ 15";
+        titleString[15] = "หัวข้อที่ 16";
+        titleString[16] = "หัวข้อที่ 17";
+        titleString[17] = "หัวข้อที่ 18";
+        titleString[18] = "หัวข้อที่ 19";
+        titleString[19] = "หัวข้อที่ 20";
+        //For Detail
+        String[] detailString = getResources().getStringArray(R.array.detail_short);
+
+        MyAdpter objMyAdpter = new MyAdpter(MainActivity.this, titleString, detailString, intIcon);
+        trafficListView.setAdapter(objMyAdpter);
+    }   //ListViewController
 
     private void buttonController() {
         aboutMeButton.setOnClickListener(new View.OnClickListener() {
@@ -43,7 +87,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void bindWidget() {
         trafficListView = (ListView) findViewById(R.id.listView);
-        aboutMeButton = (Button) findViewById(R. id.button);
+        aboutMeButton = (Button) findViewById(R.id.button);
     }
 
 } // Main Class นี่คือคลาสหลัก
---
+++ app/src/main/java/apprtc/nan/mytraffic/MyAdpter.java
@@ -0,0 +1,65 @@
+package apprtc.nan.mytraffic;
+
+import android.content.Context;
+import android.media.Image;
+import android.view.LayoutInflater;
+import android.view.TextureView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Objects;
+
+/**
+ * Created by com on 6/12/2558.
+ */
+public class MyAdpter extends BaseAdapter {
+
+    //Explicit
+    private Context objContext;
+    private String[] titleStrings, detalStrings;
+    private int[] iconInts;
+
+    public MyAdpter(Context objContext, String[] titleStrings, String[] detalStrings, int[] iconInts) {
+        this.objContext = objContext;
+        this.titleStrings = titleStrings;
+        this.detalStrings = detalStrings;
+        this.iconInts = iconInts;
+    } // Constructor
+
+
+    @Override
+    public int getCount() {
+        return titleStrings.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+
+        //Open Service
+        LayoutInflater objLayoutInflater = (LayoutInflater) objContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view1 = objLayoutInflater.inflate(R.layout.my_listview, viewGroup, false);
+        //Setup TextView
+        TextView titleTextView = (TextView) view1.findViewById(R.id.textView2);
+        titleTextView.setText(titleStrings[1]);
+        TextView detailTextView = (TextView) view1.findViewById(R.id.textView3);
+        detailTextView.setText(detalStrings[i]);
+
+        //Set Image
+        ImageView iconImageView = (ImageView) view1.findViewById(R.id.imageView);
+        iconImageView.setImageResource(iconInts[i]);
+        return view1;
+    }
+}   // Main Class
